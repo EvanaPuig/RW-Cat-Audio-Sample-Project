ecd0ee60e20e6d479c0ac6cae5096db8716b783d
evana <evanamp233@gmail.com>
2020-06-07 13:29:22 -0500

    Adds a tab bar, and a recycler view for showing the audios recorded

---
+++ app/src/main/java/com/raywenderlich/android/cat_audio/ui/RecordingListFragment.kt
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2020 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ * distribute, sublicense, create a derivative work, and/or sell copies of the
+ * Software in any work that is designed, intended, or marketed for pedagogical or
+ * instructional purposes related to programming, coding, application development,
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works,
+ * or sale is expressly withheld.
+ *
+ * This project and source code may use libraries or frameworks that are
+ * released under various Open-Source licenses. Use of those libraries and
+ * frameworks are governed by their own individual licenses.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.raywenderlich.android.cat_audio.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.raywenderlich.android.cat_audio.adapter.MyItemRecyclerViewAdapter
+import com.raywenderlich.android.cataudio.R
+import com.raywenderlich.android.cat_audio.dummy.DummyContent
+
+/**
+ * A fragment representing a list of Items.
+ */
+class RecordingListFragment : Fragment() {
+
+  private var columnCount = 1
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    arguments?.let {
+      columnCount = it.getInt(
+          ARG_COLUMN_COUNT)
+    }
+  }
+
+  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    val view = inflater.inflate(R.layout.fragment_recording_list_container, container, false)
+
+    // Set the adapter
+    if (view is RecyclerView) {
+      with(view) {
+        layoutManager = when {
+          columnCount <= 1 -> LinearLayoutManager(context)
+          else -> GridLayoutManager(context, columnCount)
+        }
+        adapter = MyItemRecyclerViewAdapter(
+            DummyContent.ITEMS)
+      }
+    }
+    return view
+  }
+
+  companion object {
+
+    // TODO: Customize parameter argument names
+    const val ARG_COLUMN_COUNT = "column-count"
+
+    // TODO: Customize parameter initialization
+    @JvmStatic
+    fun newInstance(columnCount: Int) =
+        RecordingListFragment().apply {
+          arguments = Bundle().apply {
+            putInt(
+                ARG_COLUMN_COUNT, columnCount)
+          }
+        }
+  }
+}
