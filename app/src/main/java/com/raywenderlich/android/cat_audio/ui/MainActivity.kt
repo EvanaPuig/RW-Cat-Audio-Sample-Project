2ab3f7e52d0fbc1b900699199ed3857b685ad790
evana <evanamp233@gmail.com>
2020-06-07 13:24:39 -0500

    Adds a tab bar, and a recycler view for showing the audios recorded

--- app/src/main/java/com/raywenderlich/android/cat_audio/ui/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/android/cat_audio/ui/MainActivity.kt
@@ -1,16 +1,16 @@
 /*
  * Copyright (c) 2020 Razeware LLC
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
  * distribute, sublicense, create a derivative work, and/or sell copies of the
  * Software in any work that is designed, intended, or marketed for pedagogical or
@@ -18,11 +18,11 @@
  * or information technology.  Permission for such use, copying, modification,
  * merger, publication, distribution, sublicensing, creation of derivative works,
  * or sale is expressly withheld.
- * 
+ *
  * This project and source code may use libraries or frameworks that are
  * released under various Open-Source licenses. Use of those libraries and
  * frameworks are governed by their own individual licenses.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -32,10 +32,17 @@
  * THE SOFTWARE.
  */
 
-package com.raywenderlich.android.cataudio
+package com.raywenderlich.android.cat_audio.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationMenu
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.raywenderlich.android.cataudio.R
 
 /**
  * Main Screen
@@ -49,8 +56,17 @@ class MainActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
-    // Your code
+    val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
+    val navController = findNavController(R.id.nav_host_fragment)
+
+    val appBarConfiguration = AppBarConfiguration(
+        setOf(
+            R.id.navigation_record_fragment, R.id.navigation_recording_list
+        )
+    )
+    setupActionBarWithNavController(navController, appBarConfiguration)
+    navView.setupWithNavController(navController)
 
   }
 }
